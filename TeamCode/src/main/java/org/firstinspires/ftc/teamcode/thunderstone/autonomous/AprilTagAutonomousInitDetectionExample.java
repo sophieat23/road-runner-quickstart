c87aa458b22c388874fe10d0a7ebea6f069d6f1a
Joshmo23 <joshmo23@sssas.org>
2023-01-10 16:39:31 -0500

    April tags moved to team auto file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thunderstone/autonomous/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thunderstone/autonomous/AprilTagAutonomousInitDetectionExample.java
@@ -19,7 +19,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode.thunderstone.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -29,7 +29,6 @@ import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thunderstone/autonomous/AprilTagDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thunderstone/autonomous/AprilTagDetectionPipeline.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode.thunderstone.autonomous;
 /*
  * Copyright (c) 2021 OpenFTC Team
  *
