fb915f3eb2aeb5fc29eee6896bf0ecefa8efc924
lilyhu23 <72104450+lilyhu23@users.noreply.github.com>
2023-01-14 11:15:22 -0500

    testing new auton code path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,58 +15,69 @@ public class MeepMeepTesting {
                 .setConstraints(45.48291908330528, 45.48291908330528, Math.toRadians(180), Math.toRadians(180), 11.5)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, -61.5, Math.toRadians(90)))
-                                .lineToSplineHeading(new Pose2d(-34, -16., Math.toRadians(90)))
-                                .splineTo(new Vector2d(-29, -11), Math.toRadians(45))
-                                //^this would simulate going infornt of the pole and raising the
-                                //raise life
-                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
-                                //drop cone
-                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
-                                //^this would be after the thing rising
-                                //lower lift
-                                //below is 4stack
-                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
-                                //^go to infront of cones
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-60.5,-12, Math.toRadians(180)))
-                                //^go to ontop of cone
-                                //lower lift
-                                //get cone
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
-                                //lower lift
-                                .lineToSplineHeading(new Pose2d(-29, -11, Math.toRadians(45)))
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
-                                //drop cone
-                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
-                                //^this would be after the thing rising
-                                //lower lift
-                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
-                                //^go to infront of cones
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-60.5,-12, Math.toRadians(180)))
-                                //^go to ontop of cone
-                                //lower lift
-                                //get cone
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
-                                //lower lift
-                                .lineToSplineHeading(new Pose2d(-29, -11, Math.toRadians(45)))
-                                //raise lift
-                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
-                                //drop cone
-                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
-                                //^this would be after the thing rising
-                                //lower lift
+                                .forward(55)
+                                .back(30)
+                                .turn(-.75,39,39)
+                                .forward(10)
+                                //lift code here to middle height
 
 
-                                //.lineToSplineHeading(new Pose2d(-11, -11, Math.toRadians(90)))
-                                //ZONE1
-                                //.lineToSplineHeading(new Pose2d(-34, -11, Math.toRadians(90)))
-                                //ZONE2
-                                //.lineToSplineHeading(new Pose2d(-57, -11, Math.toRadians(90)))
-                                //ZONE3
+
+
+
+
+//                                .lineToSplineHeading(new Pose2d(-34, -16., Math.toRadians(90)))
+//                                .splineTo(new Vector2d(-29, -11), Math.toRadians(45))
+//                                //^this would simulate going infornt of the pole and raising the
+//                                //raise life
+//                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
+//                                //drop cone
+//                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
+//                                //^this would be after the thing rising
+//                                //lower lift
+//                                //below is 4stack
+//                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
+//                                //^go to infront of cones
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-60.5,-12, Math.toRadians(180)))
+//                                //^go to ontop of cone
+//                                //lower lift
+//                                //get cone
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
+//                                //lower lift
+//                                .lineToSplineHeading(new Pose2d(-29, -11, Math.toRadians(45)))
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
+//                                //drop cone
+//                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
+//                                //^this would be after the thing rising
+//                                //lower lift
+//                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
+//                                //^go to infront of cones
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-60.5,-12, Math.toRadians(180)))
+//                                //^go to ontop of cone
+//                                //lower lift
+//                                //get cone
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-58.5,-12, Math.toRadians(180)))
+//                                //lower lift
+//                                .lineToSplineHeading(new Pose2d(-29, -11, Math.toRadians(45)))
+//                                //raise lift
+//                                .lineToSplineHeading(new Pose2d(-29, -5, Math.toRadians(45)))
+//                                //drop cone
+//                                .lineToSplineHeading(new Pose2d(-31, -11, Math.toRadians(45)))
+//                                //^this would be after the thing rising
+//                                //lower lift
+//
+//
+//                                //.lineToSplineHeading(new Pose2d(-11, -11, Math.toRadians(90)))
+//                                //ZONE1
+//                                //.lineToSplineHeading(new Pose2d(-34, -11, Math.toRadians(90)))
+//                                //ZONE2
+//                                //.lineToSplineHeading(new Pose2d(-57, -11, Math.toRadians(90)))
+//                                //ZONE3
 
 
                                 .build()
